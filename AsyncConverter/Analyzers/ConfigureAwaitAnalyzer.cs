48bd1f1b6fc9688ba97033b1c87dcba97f21d9d4
i.mamay <i.mamay@skbkontur.ru>
2017-02-11 18:45:38 +0500

    add new Highlighting and QuickFix

---
+++ AsyncConverter/Analyzers/ConfigureAwaitAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using AsyncConverter.Highlightings;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.CSharp.PostfixTemplates;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace AsyncConverter.Analyzers
+{
+    [ElementProblemAnalyzer(typeof(IAwaitExpression), HighlightingTypes = new[] {typeof(ConfigureAwaitHighlighting) })]
+    public class ConfigureAwaitAnalyzer : ElementProblemAnalyzer<IAwaitExpression>
+    {
+        protected override void Run(IAwaitExpression element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            var declaredType = element.Task?.GetExpressionType() as IDeclaredType;
+            if(!declaredType.IsConfigurableAwaitable() && !declaredType.IsGenericConfigurableAwaitable())
+                consumer.AddHighlighting(new ConfigureAwaitHighlighting(element));
+        }
+    }
+}
---
+++ AsyncConverter/QuickFixes/ConfigureAwaitAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AsyncConverter.Highlightings;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.TextControl;
+
+namespace AsyncConverter.QuickFixes
+{
+    public class ConfigureAwaitAction : BulbActionBase
+    {
+        private readonly ConfigureAwaitHighlighting configureAwaitHighlighting;
+        private readonly bool configureAwaitValue;
+
+        public ConfigureAwaitAction(ConfigureAwaitHighlighting configureAwaitHighlighting, bool configureAwaitValue)
+        {
+            this.configureAwaitHighlighting = configureAwaitHighlighting;
+            this.configureAwaitValue = configureAwaitValue;
+        }
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            var awaitExpression = configureAwaitHighlighting.AwaitExpression;
+            var psiModule = awaitExpression.GetPsiModule();
+            var factory = CSharpElementFactory.GetInstance(psiModule);
+            var taskWithConfiguring = factory.CreateExpression($"$0.ConfigureAwait({GetConfigureAwaitValueText()})", awaitExpression.Task);
+            awaitExpression.Task.ReplaceBy(taskWithConfiguring);
+            return null;
+        }
+
+        public override string Text => $"Add ConfigureAwait({GetConfigureAwaitValueText()})";
+
+        private string GetConfigureAwaitValueText() => configureAwaitValue ? "true" : "false";
+    }
+}
