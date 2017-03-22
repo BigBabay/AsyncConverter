e46c53decddc23cccc9e335074ce1b279079414c
i.mamay <i.mamay@skbkontur.ru>
2017-03-22 21:38:35 +0500

    QuickFix template

---
+++ AsyncConverter/QuickFixes/CanBeUseAsyncMethodQuickFix.cs
@@ -0,0 +1,32 @@
+using System;
+using AsyncConverter.Highlightings;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.QuickFixes;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace AsyncConverter.QuickFixes
+{
+    [QuickFix]
+    public class CanBeUseAsyncMethodQuickFix : QuickFixBase
+    {
+        private readonly CanBeUseAsyncMethodHighlighting canBeUseAsyncMethodHighlighting;
+
+        public CanBeUseAsyncMethodQuickFix(CanBeUseAsyncMethodHighlighting canBeUseAsyncMethodHighlighting)
+        {
+            this.canBeUseAsyncMethodHighlighting = canBeUseAsyncMethodHighlighting;
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            return null;
+        }
+
+        public override string Text => "Convert to async call";
+        public override bool IsAvailable(IUserDataHolder cache)
+        {
+            return canBeUseAsyncMethodHighlighting.IsValid();
+        }
+    }
+}
