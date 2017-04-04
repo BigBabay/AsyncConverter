d023482f408d3146ac510bb1c94bbed82342fc2f
i.mamay <i.mamay@skbkontur.ru>
2017-04-04 23:36:04 +0500

    AsyncAwaitMayBeElidedAnalyzer

---
+++ AsyncConverter/Analyzers/AsyncAwaitMayBeElidedAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using AsyncConverter.Highlightings;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.CSharp.Util;
+
+namespace AsyncConverter.Analyzers
+{
+    [ElementProblemAnalyzer(typeof(IAwaitExpression), HighlightingTypes = new[] {typeof(AsyncAwaitMayBeElidedHighlighting) })]
+    public class AsyncAwaitMayBeElidedAnalyzer : ElementProblemAnalyzer<IAwaitExpression>
+    {
+        protected override void Run(IAwaitExpression element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            var expression = element.Parent as IExpressionStatement;
+            if(expression == null)
+                return;
+
+            if (expression.GetNextStatement() != null)
+                return;
+
+            if(expression.GetContainingNode<IUsingStatement>() != null)
+                return;
+
+            if (expression.GetContainingNode<ITryStatement>() != null)
+                return;
+
+            consumer.AddHighlighting(new AsyncAwaitMayBeElidedHighlighting(element));
+        }
+    }
+}
