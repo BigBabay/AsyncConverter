83b021f6bd764887103c3f5a71a24db34f9b6675
i.mamay <i.mamay@skbkontur.ru>
2017-05-11 22:02:48 +0500

    tests

---
+++ AsyncConverter.Tests/QuickFixes/ArgumentValueAsTaskTests.cs
@@ -0,0 +1,9 @@
+﻿using AsyncConverter.QuickFixes;
+
+namespace AsyncConverter.Tests.QuickFixes
+{
+    public class ArgumentValueAsTaskTests : QuickFixBaseTests<ArgumentValueAsTask>
+    {
+        protected override string RelativeTestDataPath => @"ArgumentValueAsTaskTests";
+    }
+}
