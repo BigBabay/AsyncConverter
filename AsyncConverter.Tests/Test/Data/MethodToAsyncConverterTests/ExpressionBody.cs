a3ae61b4452f94711d207e9e045e57fee06b937a
i.mamay <i.mamay@skbkontur.ru>
2017-05-16 21:53:54 +0500

    ExpressionBody test

---
+++ AsyncConverter.Tests/Test/Data/MethodToAsyncConverterTests/ExpressionBody.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public void {caret}Test() => Method();
+
+        public void Method()
+        {
+        }
+
+        public Task MethodAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
