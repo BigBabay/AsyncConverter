f6dd3b897196f1be14069f5a328a0f7509d187f4
i.mamay <i.mamay@skbkontur.ru>
2017-05-25 09:14:39 +0500

    adding settings

---
+++ AsyncConverter/Settings/AsyncConverterSettings.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Resources.Settings;
+
+namespace AsyncConverter.Settings
+{
+    [SettingsKey(typeof(CodeInspectionSettings), "Settings for AsyncConverter plugin.")]
+    public class AsyncConverterSettings
+    {
+        [SettingsEntry(false, "Suggest adding 'Async' suffix to test method.")]
+        public bool ExcludeTestMethodsFromAnalysis { get; set; }
+    }
+}
