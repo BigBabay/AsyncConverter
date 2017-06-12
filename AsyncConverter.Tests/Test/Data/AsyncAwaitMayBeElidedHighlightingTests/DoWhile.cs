dece7bf47a2ab506558c183850b094979695664a
i.mamay <i.mamay@skbkontur.ru>
2017-06-12 14:46:05 +0500

    tests on bug

---
+++ AsyncConverter.Tests/Test/Data/AsyncAwaitMayBeElidedHighlightingTests/DoWhile.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            var i = 5;
+            do
+            {
+                await Task.Delay(1000).ConfigureAwait(false);
+                i--;
+            } while (i > 0);
+        }
+    }
+}
