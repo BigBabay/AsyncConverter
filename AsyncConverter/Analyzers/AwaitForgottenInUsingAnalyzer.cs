ad7c93097adcee9a615ac433bb0281329c2115fd
i.mamay <i.mamay@skbkontur.ru>
2017-06-22 21:16:02 +0500

    Analyzer template

---
+++ AsyncConverter/Analyzers/AwaitForgottenInUsingAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using AsyncConverter.Highlightings;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace AsyncConverter.Analyzers
+{
+    [ElementProblemAnalyzer(typeof(IUsingStatement), HighlightingTypes = new[] {typeof(AwaitForgottenInUsingHighlighting) })]
+    public class AwaitForgottenInUsingAnalyzer : ElementProblemAnalyzer<IUsingStatement>
+    {
+        protected override void Run(IUsingStatement element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            //var awaitElideChecker = element.GetSolution().GetComponent<IAwaitElideChecker>();
+            if (element.Declaration.IsVar)
+            {
+                var variableDeclaration = element.Declaration.Declarators.FirstOrDefault();
+                if(variableDeclaration == null)
+                    return;
+                var variableDeclarationType = variableDeclaration.Type;
+            }
+
+            //consumer.AddHighlighting(new AsyncAwaitMayBeElidedHighlighting(awaitExpression));
+        }
+    }
+}
