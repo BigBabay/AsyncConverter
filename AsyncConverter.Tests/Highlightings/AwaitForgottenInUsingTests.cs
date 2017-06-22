2ca06da1ee462fe611f8cadd6000847e62e6ecc3
i.mamay <i.mamay@skbkontur.ru>
2017-06-22 21:16:21 +0500

    fist tests

---
+++ AsyncConverter.Tests/Highlightings/AwaitForgottenInUsingTests.cs
@@ -0,0 +1,7 @@
+namespace AsyncConverter.Tests.Highlightings
+{
+    public class AwaitForgottenInUsingTests : HighlightingsTestsBase
+    {
+        protected override string Folder => "AwaitForgottenInUsingHighlightingTests";
+    }
+}
---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AwaitForgottenInUsingHighlightingTests/AsyncMethod.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            using (MethodAsync())
+            {
+
+            }
+        }
+
+        private Task<IDisposable> MethodAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
