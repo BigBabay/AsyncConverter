7054741e82187312857de6d8a5eee1607333d377
i.mamay <i.mamay@skbkontur.ru>
2017-06-24 13:41:50 +0500

    test on overload

---
+++ AsyncConverter.Tests/Test/Data/NamingQuickFix/Overload.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public interface ICalculator
+    {
+        Task<int> Sum(int first, int second);
+        Task<int> Sum(int[] values);
+    }
+
+    public class Calculator
+    {
+        public async Task<int> Sum(int first, int second)
+        {
+            await Task.Delay(10);
+            return first + second;
+        }
+
+        public async Task<int> Su{caret}m(int[] values)
+        {
+            await Task.Delay(10);
+            return values.Sum();
+        }
+    }
+}
