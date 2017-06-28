679398b07226ae6fcab501f7468203d30b53bfdd
i.mamay <i.mamay@skbkontur.ru>
2017-06-28 23:36:53 +0500

    settings

--- AsyncConverter/Settings/AsyncConverterOptionsPage.cs
+++ AsyncConverter/Settings/AsyncConverterOptionsPage.cs
@@ -1,22 +1,18 @@
 ﻿using JetBrains.Annotations;
-using JetBrains.Application.Interop.NativeHook;
 using JetBrains.DataFlow;
 using JetBrains.ReSharper.Feature.Services.Daemon.OptionPages;
 using JetBrains.ReSharper.Feature.Services.Resources;
-using JetBrains.UI.Application;
 using JetBrains.UI.Options;
 using JetBrains.UI.Options.OptionsDialog2.SimpleOptions;
-using JetBrains.UI.Validation;
 
 namespace AsyncConverter.Settings
 {
-    [OptionsPage(pageId, "AsyncSuffix", typeof(ServicesThemedIcons.AnalyzeThis), ParentId = CodeInspectionPage.PID)]
+    [OptionsPage(pageId, "AsyncConverter", typeof(ServicesThemedIcons.AnalyzeThis), ParentId = CodeInspectionPage.PID)]
     public sealed class AsyncConverterOptionsPage : CustomSimpleOptionsPage
     {
         private const string pageId = "AsyncConverter";
 
-        public AsyncConverterOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store,
-                                         IWindowsHookManager windowsHookManager, FormValidators formValidators, IUIApplication iuiApplication)
+        public AsyncConverterOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store)
             : base(lifetime, store)
         {
             AddHeader("Tests");
---
+++ AsyncConverter/Settings/AsyncSuffixSettingsAccessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+
+namespace AsyncConverter.Settings
+{
+    internal static class AsyncSuffixSettingsAccessor
+    {
+        [NotNull]
+        public static readonly Expression<Func<AsyncConverterSettings, bool>>
+            ExcludeTestMethodsFromAnalysis = x => x.ExcludeTestMethodsFromAnalysis;
+    }
+}
