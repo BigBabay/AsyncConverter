b3b5c9f8d35ea6f8f58be190f72383e3e17968b9
i.mamay <i.mamay@skbkontur.ru>
2017-07-02 23:52:08 +0500

    new settings page

--- AsyncConverter/Settings/AsyncConverterNamingOptionsPage.cs
+++ AsyncConverter/Settings/AsyncConverterNamingOptionsPage.cs
@@ -1,23 +1,11 @@
 ﻿using JetBrains.Annotations;
 using JetBrains.DataFlow;
-using JetBrains.ReSharper.Feature.Services.Daemon.OptionPages;
 using JetBrains.ReSharper.Feature.Services.Resources;
 using JetBrains.UI.Options;
 using JetBrains.UI.Options.OptionsDialog2.SimpleOptions;
 
 namespace AsyncConverter.Settings
 {
-    [OptionsPage(PID, "AsyncConverter", typeof(ServicesThemedIcons.AnalyzeThis), ParentId = CodeInspectionPage.PID)]
-    public sealed class AsyncConverterOptionsPage : CustomSimpleOptionsPage
-    {
-        public const string PID = "AsyncConverter";
-
-        public AsyncConverterOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store)
-            : base(lifetime, store)
-        {
-        }
-    }
-
     [OptionsPage(PID, "AsyncConverterNaming", typeof(ServicesThemedIcons.AnalyzeThis), ParentId = AsyncConverterOptionsPage.PID)]
     public sealed class AsyncConverterNamingOptionsPage : CustomSimpleOptionsPage
     {
