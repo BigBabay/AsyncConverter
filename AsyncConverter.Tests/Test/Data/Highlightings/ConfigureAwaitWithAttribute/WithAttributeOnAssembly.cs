cfa6b2cea46908795dd6afebaebafc20a3d64fa0
i.mamay <i.mamay@skbkontur.ru>
2017-07-09 22:51:45 +0500

    tests on assyambly

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/ConfigureAwaitWithAttribute/WithAttributeOnAssembly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+[assembly: AsyncConverter.Tests.Test.Data.Highlightings.ConfigureAwaitWithAttribute.MyCustom]
+
+namespace AsyncConverter.Tests.Test.Data.Highlightings.ConfigureAwaitWithAttribute
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            await Task.Delay(1000);
+        }
+    }
+
+    public class MyCustomAttribute : Attribute
+    {
+
+    }
+}
