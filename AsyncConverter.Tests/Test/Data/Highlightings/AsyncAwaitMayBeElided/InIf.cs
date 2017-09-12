b3a21aed56433e69ad19e652915fa1ff5ef48b0b
i.mamay <mamay.igor@gmail.com>
2017-09-12 11:13:48 +0500

    tests on Elided In If

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AsyncAwaitMayBeElided/InIf.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task SomeFuncAsync(bool foo)
+        {
+            if (foo)
+                await BarAsync().ConfigureAwait(false);
+        }
+
+        public Task BarAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
