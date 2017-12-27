0f9ffff441713c0bdebf98bbc8bc72da456bec64
i.mamay <mamay.igor@gmail.com>
2017-12-27 21:46:51 +0500

    ToHashSet EnumerableExtension

---
+++ AsyncConverter/Helpers/EnumerableExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace AsyncConverter.Helpers
+{
+    public static class EnumerableExtension
+    {
+        [NotNull]
+        public static HashSet<TItem> ToHashSet<TItem>([NotNull] this IEnumerable<TItem> items) => new HashSet<TItem>(items);
+    }
+}
