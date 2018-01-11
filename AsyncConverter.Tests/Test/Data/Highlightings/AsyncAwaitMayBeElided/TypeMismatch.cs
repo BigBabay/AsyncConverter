56cec2207a2d71beca6381992b79be93a463263f
Мамай Игорь <i.mamay@skbkontur.ru>
2018-01-11 20:16:49 +0500

    test on type missmatch

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AsyncAwaitMayBeElided/TypeMismatch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task<IList<int>> TestAsync()
+        {
+            var listTask = Task.FromResult(new List<int>());
+            return await listTask.ConfigureAwait(false);
+        }
+    }
+}
