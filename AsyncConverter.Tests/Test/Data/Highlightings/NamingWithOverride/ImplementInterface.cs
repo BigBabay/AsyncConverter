578a18fd2fd6e445b99c7c1a5b42f9bb382d3309
Мамай Игорь <i.mamay@skbkontur.ru>
2018-03-13 00:40:21 +0500

    first test on override from Interface in external assembly

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/NamingWithOverride/ImplementInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using LibraryToOverride;
+
+namespace AsyncConverter.Tests
+{
+    public class Class : IInterfaceToOverride
+    {
+        public Task Method()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
