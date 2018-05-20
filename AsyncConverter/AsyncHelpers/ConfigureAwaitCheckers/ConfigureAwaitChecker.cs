8de93481a3394a60dca4d7b2ef39176257a57e7a
Мамай Игорь <i.mamay@skbkontur.ru>
2018-05-20 16:07:23 +0500

    decompose ConfigureAwaitCheckers

---
+++ AsyncConverter/AsyncHelpers/ConfigureAwaitCheckers/ConfigureAwaitChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AsyncConverter.AsyncHelpers.ConfigureAwaitCheckers.CustomCheckers;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace AsyncConverter.AsyncHelpers.ConfigureAwaitCheckers
+{
+    [SolutionComponent]
+    internal class ConfigureAwaitChecker : IConfigureAwaitChecker
+    {
+        private readonly IConfigureAwaitCustomChecker[] awaitCustomCheckers;
+
+        public ConfigureAwaitChecker(IEnumerable<IConfigureAwaitCustomChecker> awaitCustomCheckers)
+        {
+            this.awaitCustomCheckers = awaitCustomCheckers.ToArray();
+        }
+
+        public bool NeedAdding(IAwaitExpression element)
+        {
+            return awaitCustomCheckers.All(x => x.NeedAdding(element));
+        }
+    }
+}
---
+++ AsyncConverter/AsyncHelpers/ConfigureAwaitCheckers/CustomCheckers/AttributeTypeChecker.cs
@@ -0,0 +1,18 @@
+﻿using AsyncConverter.Checkers;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace AsyncConverter.AsyncHelpers.ConfigureAwaitCheckers.CustomCheckers
+{
+    [SolutionComponent]
+    class AttributeTypeChecker : IConfigureAwaitCustomChecker
+    {
+        public bool NeedAdding(IAwaitExpression element)
+        {
+            var attributeTypeChecker = element.GetSolution().GetComponent<IAttributeTypeChecker>();
+
+            return !attributeTypeChecker.IsUnder(element);
+        }
+    }
+}
