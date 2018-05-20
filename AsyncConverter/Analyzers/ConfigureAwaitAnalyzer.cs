b7d0247ef6d6982a06ec66aaa4eb2801546b02d3
Мамай Игорь <i.mamay@skbkontur.ru>
2018-05-20 16:07:23 +0500

    decompose ConfigureAwaitCheckers

--- AsyncConverter/Analyzers/ConfigureAwaitAnalyzer.cs
+++ AsyncConverter/Analyzers/ConfigureAwaitAnalyzer.cs
@@ -1,10 +1,7 @@
-﻿using AsyncConverter.AsyncHelpers.Checker;
-using AsyncConverter.Checkers;
+﻿using AsyncConverter.AsyncHelpers.ConfigureAwaitCheckers;
 using AsyncConverter.Highlightings;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.CSharp.PostfixTemplates;
 using JetBrains.ReSharper.Feature.Services.Daemon;
-using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
 
@@ -17,30 +14,10 @@ namespace AsyncConverter.Analyzers
         {
             var needConfAwaitCheckers = element.GetSolution().GetComponent<IConfigureAwaitChecker>();
 
-            var attributeFunctionChecker = element.GetSolution().GetComponent<IAttributeFunctionChecker>();
-            var attributeTypeChecker = element.GetSolution().GetComponent<IAttributeTypeChecker>();
-
-            var declaredType = element.Task?.GetExpressionType() as IDeclaredType;
-
-            if (declaredType.IsConfigurableAwaitable() || declaredType.IsGenericConfigurableAwaitable())
-                return;
-
-            if (attributeFunctionChecker.IsUnder(element))
-                return;
-
-            if (attributeTypeChecker.IsUnder(element))
+            if(!needConfAwaitCheckers.NeedAdding(element))
                 return;
 
             consumer.AddHighlighting(new ConfigureAwaitHighlighting(element));
         }
-    }
-
-    public interface IConfigureAwaitChecker
-    {
-        bool NeedAdding();
-    }
-
-    class ConfigureAwaitChecker : IConfigureAwaitChecker
-    {
     }
 }
---
+++ AsyncConverter/AsyncHelpers/ConfigureAwaitCheckers/CustomCheckers/AttributeFunctionChecker.cs
@@ -0,0 +1,22 @@
+﻿using AsyncConverter.AsyncHelpers.Checker;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace AsyncConverter.AsyncHelpers.ConfigureAwaitCheckers.CustomCheckers
+{
+    [SolutionComponent]
+    internal class AttributeFunctionChecker: IConfigureAwaitCustomChecker
+    {
+        private readonly IAttributeFunctionChecker attributeFunctionChecker;
+
+        public AttributeFunctionChecker(IAttributeFunctionChecker attributeFunctionChecker)
+        {
+            this.attributeFunctionChecker = attributeFunctionChecker;
+        }
+
+        public bool NeedAdding(IAwaitExpression element)
+        {
+            return !attributeFunctionChecker.IsUnder(element);
+        }
+    }
+}
