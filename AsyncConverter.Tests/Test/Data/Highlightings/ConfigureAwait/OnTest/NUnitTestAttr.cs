073ef27b649262758f24970e228de5a201369c95
Мамай Игорь <i.mamay@skbkontur.ru>
2018-06-25 11:43:17 +0500

    Move tests to separate folder

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/ConfigureAwait/OnTest/NUnitTestAttr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    using NUnit.Framework;
+    public class Class
+    {
+        [Test]
+        public async Task Test()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
+
+namespace NUnit.Framework
+{
+    public class TestAttribute : Attribute
+    { }
+}
