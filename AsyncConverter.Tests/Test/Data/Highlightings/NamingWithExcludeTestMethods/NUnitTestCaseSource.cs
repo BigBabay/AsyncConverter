c974ecde4ca576040d858e7c3fd19f4eedd88d35
Мамай Игорь <i.mamay@skbkontur.ru>
2018-07-10 22:12:38 +0500

    add test on TestCaseSourceAttribute

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/NamingWithExcludeTestMethods/NUnitTestCaseSource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    using NUnit.Framework;
+    public class Class
+    {
+        [TestCaseSource(nameof(FileNames))]
+        public async Task Test()
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+            await Task.Delay(1000).ConfigureAwait(false);
+        }
+
+        protected TestCaseData[] FileNames()
+        {
+            return new TestCaseData[0];
+        }
+    }
+}
+
+namespace NUnit.Framework
+{
+    public class TestCaseSourceAttribute : Attribute
+    {
+        public TestCaseSourceAttribute(string sourceName)
+        {
+
+        }
+    }
+
+    public class TestCaseData
+    {
+    }
+}
