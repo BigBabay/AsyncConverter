ea78a48ec312d6d7a83c8a7409152017a2e4d076
i.mamay <mamay.igor@gmail.com>
2018-11-30 22:21:26 +0500

    test when await in if condition

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AsyncAwaitMayBeElided/AwaitInIf.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        private async Task MethodAsync()
+        {
+            if (await IsAsync().ConfigureAwait(false))
+            {
+            }
+        }
+
+        private static async Task<bool> IsAsync()
+        {
+            await Task.Delay(100).ConfigureAwait(false);
+            return true;
+        }
+    }
+}
