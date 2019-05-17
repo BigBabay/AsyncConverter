ea96402ebe350dbb564dfb0e2dfda7e8f8ec4f11
i.mamay <i.mamay@skbkontur.ru>
2019-05-17 11:22:57 +0500

    tmp

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AsyncAwaitMayBeElided/SimpleProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
