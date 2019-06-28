cb8486f16ea4dd62a2ce895ff14a7e95f1d9f882
Igor Mamay <mamay.igor@gmail.com>
2019-06-28 15:59:36 +0500

    tests

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/ConfigureAwait/Common/ValueTask.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            await Aaa().ConfigureAwait(false);
+        }
+        
+        public static ValueTask<int> Aaa()
+        {
+            return new ValueTask<int>(5);
+        }
+    }
+}
