9c69e740594ef0b2e489438496c9eed36d969779
Igor Mamay <mamay.igor@gmail.com>
2019-07-19 14:56:25 +0500

    removed quick fix, ReSharper implemented it

--- AsyncConverter.Tests/QuickFixes/ReturnValueAsTaskTests.cs
+++
@@ -1,9 +0,0 @@
-﻿using AsyncConverter.QuickFixes;
-
-namespace AsyncConverter.Tests.QuickFixes
-{
-    public class ReturnValueAsTaskTests : QuickFixBaseTests<ReturnValueAsTask>
-    {
-        protected override string Folder => @"ReturnValueAsTask";
-    }
-}
