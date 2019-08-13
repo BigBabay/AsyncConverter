e6e81fae412a572c6d15bc1b73d8733b7c8ddc0c
mamay.igor <mamay.igor@gmail.com>
2019-08-13 14:44:06 +0500

    update to 2019.2

--- AsyncConverter.Tests/TestEnvironment.cs
+++ AsyncConverter.Tests/TestEnvironment.cs
@@ -1,10 +1,11 @@
-﻿using JetBrains.Application.BuildScript.Application.Zones;
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.TestFramework;
 using JetBrains.TestFramework;
 using JetBrains.TestFramework.Application.Zones;
 using NUnit.Framework;
 
-[assembly: RequiresSTA]
+[assembly:Apartment(ApartmentState.STA)]
 
 [ZoneDefinition]
 public class TestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone> { }
