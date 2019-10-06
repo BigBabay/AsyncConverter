bea86d9446a1a49afe4f7f811ee6c30d4552c02d
Igor <mamay.igor@gmail.com>
2019-10-06 18:45:51 +0300

    add test on await result

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/AsyncWaitTests/AwaitResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using SKBKontur.Billy.Core.Common.Threading;
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task<int> TestAsync()
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+            return Task.FromResult(1000).AwaitResult();
+        }
+    }
+}
+
+namespace SKBKontur.Billy.Core.Common.Threading
+{
+    public static class TaskExtensions
+    {
+        public static T AwaitResult<T>(this Task<T> task)
+            => task.GetAwaiter().GetResult();
+        public static void AwaitResult(this Task task)
+            => task.GetAwaiter().GetResult();
+    }    
+}
