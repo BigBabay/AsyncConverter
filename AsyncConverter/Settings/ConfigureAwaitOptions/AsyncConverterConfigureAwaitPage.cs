9903b9685cd3786e8851f69ed384e6b704aada9e
Igor <mamay.igor@gmail.com>
2019-12-15 12:19:03 +0300

    remove ConfigureAwait attributwe form

--- AsyncConverter/Settings/ConfigureAwaitOptions/AsyncConverterConfigureAwaitPage.cs
+++ AsyncConverter/Settings/ConfigureAwaitOptions/AsyncConverterConfigureAwaitPage.cs
@@ -1,59 +1,22 @@
-﻿using System.Linq;
-using JetBrains.Annotations;
-using JetBrains.Application.Settings;
-using JetBrains.Application.UI.Controls.Dialogs;
-using JetBrains.Application.UI.Controls.StringCollectionEdit.Impl;
-using JetBrains.Application.UI.Controls.StringCollectionEdit.Impl.Buttons;
-using JetBrains.Application.UI.Controls.StringCollectionEdit.Impl.Items;
+﻿using JetBrains.Annotations;
 using JetBrains.Application.UI.Options;
 using JetBrains.Application.UI.Options.OptionsDialog;
+using JetBrains.IDE.UI.Options;
 using JetBrains.Lifetimes;
 using JetBrains.ReSharper.Feature.Services.Resources;
 
 namespace AsyncConverter.Settings.ConfigureAwaitOptions
 {
     [OptionsPage(PID, "ConfigureAwait settings", typeof(ServicesThemedIcons.InspectionToolWindow), ParentId = AsyncConverterPage.PID)]
-    public sealed class AsyncConverterConfigureAwaitPage : CustomSimpleOptionsPage
+    public sealed class AsyncConverterConfigureAwaitPage : BeSimpleOptionsPage
     {
         public const string PID = "AsyncConverterConfigureAwait";
 
-        public AsyncConverterConfigureAwaitPage(Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store,
-                                                IPromptWinForm promptWinForms)
-            : base(lifetime, store)
+        public AsyncConverterConfigureAwaitPage(Lifetime lifetime, OptionsPageContext optionsPageContext, [NotNull] OptionsSettingsSmartContext store)
+            : base(lifetime, optionsPageContext, store)
         {
             AddBoolOption((AsyncConverterConfigureAwaitSettings options) => options.ExcludeTestMethodsFromConfigureAwait,
                 "Do not suggest add 'ConfigureAwait' in test method.");
-
-            AddHeader("In class and method under attributes will be ignored ConfigureAwait suggestion");
-            var editItemViewModelFactory = new DefaultCollectionEditItemViewModelFactory(null);
-            var buttonProviderFactory = new DefaultButtonProviderFactory(lifetime, promptWinForms, editItemViewModelFactory);
-            var attributeTypes = new StringCollectionEditViewModel(lifetime, "Attributes:",
-                buttonProviderFactory, editItemViewModelFactory);
-            foreach (var type in store.EnumIndexedValues(AsyncConverterSettingsAccessor.ConfigureAwaitIgnoreAttributeTypes))
-            {
-                attributeTypes.AddItem(type);
-            }
-
-            attributeTypes.Items.CollectionChanged += (o, e) =>
-                                                        {
-                                                            foreach (
-                                                                var entryIndex in
-                                                                OptionsSettingsSmartContext.EnumEntryIndices(AsyncConverterSettingsAccessor.ConfigureAwaitIgnoreAttributeTypes)
-                                                                    .ToArray())
-                                                            {
-                                                                OptionsSettingsSmartContext.RemoveIndexedValue(AsyncConverterSettingsAccessor.ConfigureAwaitIgnoreAttributeTypes,
-                                                                    entryIndex);
-                                                            }
-                                                            foreach (
-                                                                var editItemViewModel in
-                                                                attributeTypes.Items)
-                                                            {
-                                                                OptionsSettingsSmartContext.SetIndexedValue(AsyncConverterSettingsAccessor.ConfigureAwaitIgnoreAttributeTypes,
-                                                                    editItemViewModel.PresentableName, editItemViewModel.PresentableName);
-                                                            }
-                                                        };
-            AddCustomOption(attributeTypes);
-
         }
     }
 }
