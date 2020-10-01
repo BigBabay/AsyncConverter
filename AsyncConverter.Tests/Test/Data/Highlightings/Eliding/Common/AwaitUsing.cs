c3971d5475d76c7c66a1888e974ba02648938d83
Igor.Mamay <Igor.Mamay@veeam.com>
2020-10-01 12:25:36 +0300

    add using var tests

---
+++ AsyncConverter.Tests/Test/Data/Highlightings/Eliding/Common/AwaitUsing.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            await using (new MemoryStream())
+            {
+                await Task.Delay(1000).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ AsyncConverter.Tests/Test/Data/Highlightings/Eliding/Common/AwaitUsingVar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public abstract class Class
+    {
+        public async Task TestAsync()
+        {
+            await using var usingVar = GetAsyncDisposable();
+            await Task.Delay(0).ConfigureAwait(false);
+        }
+
+        public abstract IAsyncDisposable GetAsyncDisposable();
+    }
+}
