dfc196d7766c536552ed881abbd1bfa7c511972e
m7clarke <47439144+m7clarke@users.noreply.github.com>
2021-04-09 23:43:39 +1200

    Update JetBrains SDK to 2021.1

--- AsyncConverter/ContextActions/MethodToAsyncConverter.cs
+++ AsyncConverter/ContextActions/MethodToAsyncConverter.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.ContextActions;
-using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
+using JetBrains.ReSharper.Feature.Services.CSharp.ContextActions;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.TextControl;
