306e7df91cc73d3e2823e4eea074ddf929d605d1
mamay.igor <mamay.igor@gmail.com>
2023-01-08 13:09:29 +0100

    add support for default interface method

---
+++ AsyncConverter.Tests/Test/Data/ContextActions/MethodToAsyncConverterTests/RenameDefaultMethodInInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public interface IClass
+    {
+        void {caret}Test()
+        {
+            Run();
+            Run();
+        }
+
+        void Run();
+        Task RunAsync();
+    }
+}
