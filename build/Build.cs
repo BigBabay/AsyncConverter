c4212cac522c6d5ecf4dbf059a475135d9614cb9
mamay.igor <mamay.igor@gmail.com>
2023-01-12 00:42:53 +0100

    make build for rider rider

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using Nuke.Common;
 using Nuke.Common.IO;
@@ -20,28 +22,38 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    public AbsolutePath ArtifactsDirectory => RootDirectory / "packages";
+    const string riderVersion = "1.2.8.33";
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "packages";
+    AbsolutePath RiderZip => RootDirectory / "Rider" / "zip";
+    AbsolutePath RiderDir => RiderZip / "AsyncConverter.Rider";
+    AbsolutePath RiderJarDir => RiderDir / "lib";
+    AbsolutePath RiderDotnetDir => RiderDir / "dotnet";
+    AbsolutePath RiderMetaDir => RootDirectory / "Rider" / "meta";
+    AbsolutePath RiderBinDir => RootDirectory / "AsyncConverter" / "bin" / "Rider" / Configuration / "net472";
 
     Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-                  {
-                      DotNetClean();
-                      EnsureCleanDirectory(ArtifactsDirectory);
-                  });
+                        .Before(Restore)
+                        .Executes(() =>
+                                  {
+                                      DotNetClean();
+                                      EnsureCleanDirectory(ArtifactsDirectory);
+                                      DeleteFilesInDir(RiderJarDir);
+                                      DeleteFilesInDir(RiderDotnetDir);
+                                  });
 
     Target Restore => _ => _
-        .DependsOn(Clean)
-        .Executes(() =>
-                  {
-                      DotNetRestore();
-                  });
+                          .DependsOn(Clean)
+                          .Executes(() =>
+                                    {
+                                        DotNetRestore();
+                                    });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
                   {
-                      DotNetBuild();
+                      DotNetBuild(c => DotNetBuildSettingsExtensions.SetConfiguration(c, Configuration));
                   });
     Target Pack => _ => _
        .DependsOn(Compile)
@@ -53,12 +65,11 @@ class Build : NukeBuild
 
     void PackForRider()
     {
-        DotNetPack(s =>
-                   {
-                       var settings = DotNetPackSettingsExtensions.SetOutputDirectory(s, RootDirectory / "Rider" / "AsyncConverter.Rider").DisableIncludeSymbols();
-                       return DotNetPackSettingsExtensions.SetProject(settings, "AsyncConverter/AsyncConverter.Rider.csproj");
-                   });
-        ZipFile.CreateFromDirectory(RootDirectory / "Rider", ArtifactsDirectory / $"AsyncConverter.Rider.zip");
+        ZipFile.CreateFromDirectory(RiderMetaDir,
+            RiderJarDir / $"AsyncConverter.Rider-{riderVersion}.jar");
+        File.Copy(RiderBinDir / "AsyncConverter.Rider.dll",
+            RiderDotnetDir / "AsyncConverter.Rider.dll", true);
+        ZipFile.CreateFromDirectory(RiderZip, ArtifactsDirectory / $"AsyncConverter.Rider.zip");
     }
 
     void PackForReSharper() =>
@@ -67,4 +78,15 @@ class Build : NukeBuild
                        var settings = DotNetPackSettingsExtensions.SetOutputDirectory(s, ArtifactsDirectory).DisableIncludeSymbols();
                        return DotNetPackSettingsExtensions.SetProject(settings, "AsyncConverter/AsyncConverter.csproj");
                    });
+
+    void DeleteFilesInDir(AbsolutePath dir)
+    {
+        if(!Directory.Exists(dir))
+            return;
+        foreach (var file in Directory.GetFiles(dir))
+        {
+            File.Delete(file);
+        }
+    }
+
 }
